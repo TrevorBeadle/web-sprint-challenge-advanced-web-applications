44aa5d1b8506056532d2dfb3b0a7e75cad37b92f
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-16 10:16:23 -0500

    created PrivateRoute component and login method

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login")
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
