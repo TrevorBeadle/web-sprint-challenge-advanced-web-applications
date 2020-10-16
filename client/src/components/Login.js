d09f3e0402e10219dc8f69c1c55c93cc5d5a0d3d
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-16 10:24:21 -0500

    built out login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [values, setValues] = useState({
+    credentials: { username: "", password: "" },
+  });
+
   const login = e => {
     e.preventDefault();
     axiosWithAuth()
@@ -12,10 +16,35 @@ const Login = () => {
       .catch(err => console.log(err));
   };
 
+  const onChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={values.credentials.username}
+          onChange={onChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          name="password"
+          value={values.credentials.password}
+          onChange={onChange}
+        />
+      </form>
     </>
   );
 };
